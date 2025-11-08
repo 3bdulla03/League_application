f6f328100503958bea9a7cde3071ea5600e25a52
HussainM44 <hussainmumtaz2001@gmail.com>
2025-11-08 23:26:55 +0300

    Auth Complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,15 @@
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
 
-exports.users_get = async (req, res) => {
+exports.auth_get = async (req, res) => {
   res.render("auth/index.ejs")
 }
 
-exports.users_signUp_get = async (req, res) => {
+exports.auth_signUp_get = async (req, res) => {
   res.render("auth/signUp.ejs")
 }
 
-exports.users_signUp_post = async (req, res) => {
+exports.auth_signUp_post = async (req, res) => {
   const userNameConfirm = await User.findOne({ username: req.body.username })
   if (userNameConfirm) {
     res.send("Username already taken. Pls choose Other username.")
@@ -25,4 +25,30 @@ exports.users_signUp_post = async (req, res) => {
   newUser = await User.create(req.body)
 
   res.render("auth/index.ejs")
+}
+
+exports.auth_signIn_get = async (req, res) => {
+  res.render("auth/signIn.ejs")
+}
+
+exports.auth_signIn_post = async (req, res) => {
+  const user = await User.findOne({ username: req.body.username })
+  if (!user) {
+    return res.send("Invalid Username or Password , Try Again !!!")
+  }
+  const verifyPassword = bcrypt.compareSync(req.body.password, user.password)
+  if (!verifyPassword) {
+    return res.send("Invalid Username or Password , Try Again !!!")
+  }
+
+  req.session.user = {
+    username: user.username,
+    _id: user._id,
+  }
+  res.render("auth/index.ejs") // It should render to next page(maybe leagues page)
+}
+
+exports.auth_signOut_get = async (req, res) => {
+  req.session.destroy()
+  res.render("mainPage.ejs")
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 const router = require("express").Router()
 const authCont = require("../controllers/auth")
 
-router.get("/", authCont.users_get)
-router.get("/sign-up", authCont.users_signUp_get)
-router.post("/sign-up", authCont.users_signUp_post)
+router.get("/", authCont.auth_get)
+router.get("/sign-up", authCont.auth_signUp_get)
+router.post("/sign-up", authCont.auth_signUp_post)
+router.get("/sign-in", authCont.auth_signIn_get)
+router.post("/sign-in", authCont.auth_signIn_post)
+router.get("/sign-out",authCont.auth_signOut_get)
 
 //
 module.exports = router
