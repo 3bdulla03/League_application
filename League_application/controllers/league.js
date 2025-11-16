3d82d84d35760b9ae32af9ad7d203f2ed41a0cda
[200~hassanali121212w-svg~ <hassanali121212w@gmail.com>
2025-11-16 19:01:53 +0300

    teams controllers routes and views

---
+++ League_application/controllers/league.js
@@ -0,0 +1,30 @@
+const League = require("../models/league")
+const Team = require("../models/team")
+
+
+exports.league_index_get = async (req, res) => {
+    const league = await League.find().populate('manager')
+    res.render("league/index.ejs", {league})
+}
+
+exports.league_create_get = async (req, res) => {
+    // const league = await League.find().populate('user')
+    res.render("league/new.ejs")
+}
+
+exports.league_create_post = async (req, res) => {
+    req.body.manager = req.session.user._id
+    req.body.team = 
+    await League.create(req.body)
+    res.redirect("/league")
+}
+
+exports.league_show_get = async (req, res) => {
+    const league = await League.findById(req.params.leagueId).populate('team')
+    res.render("league/show.ejs", {league})
+}
+
+exports.league_delete_delete = async (req, res) => {
+    const league = await League.findByIdAndDelete(req.params.leagueId)
+    res.redirect("/league")
+}
