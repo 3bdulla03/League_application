b0ae996dab0f8b85740d9854627d366e8cecad39
HussainM44 <hussainmumtaz2001@gmail.com>
2025-11-18 18:36:40 +0300

    trade player

--- controllers/player.js
+++ controllers/player.js
@@ -1,7 +1,6 @@
-const Team = require('../models/team.js')
-const Player = require('../models/player.js')
-const League = require('../models/league.js')
-
+const Team = require("../models/team.js")
+const Player = require("../models/player.js")
+const League = require("../models/league.js")
 
 // //index
 // exports.player_index_get = async(req,res)=>{
@@ -11,16 +10,15 @@ const League = require('../models/league.js')
 //   res.render('player/index.ejs', {players})
 // }
 
-
 //create
-exports.player_new_get = async(req,res)=>{
+exports.player_new_get = async (req, res) => {
   //view should send team Id throw url using url-parameter
   const league = await League.findById(req.params.leagueId)
   const team = await Team.findById(req.params.teamId) //searching for the team in database
-  res.render('player/new.ejs', {league, team}) //load create view
+  res.render("player/new.ejs", { league, team }) //load create view
 }
 
-exports.player_new_post = async(req,res)=>{
+exports.player_new_post = async (req, res) => {
   //create player functionality
   const team = await Team.findById(req.params.teamId)
   req.body.team = req.params.teamId //adding the team id to the player object
@@ -28,33 +26,60 @@ exports.player_new_post = async(req,res)=>{
   team.players.push(newPlayer._id)
   newPlayer.save()
   team.save()
-  res.redirect(`/leagues/${req.params.leagueId}/teams/${req.params.teamId}`)//redirect to players/index
+  res.redirect(`/leagues/${req.params.leagueId}/teams/${req.params.teamId}`) //redirect to players/index
 }
 
 //show one
-exports.player_show_get = async(req,res)=>{
+exports.player_show_get = async (req, res) => {
   const league = await League.findById(req.params.leagueId)
   const team = await Team.findById(req.params.teamId)
-  const player = await Player.findById(req.params.playerId).populate('team')
-  res.render('player/show.ejs', {player , league, team})
+  const player = await Player.findById(req.params.playerId).populate("team")
+  res.render("player/show.ejs", { player, league, team })
 }
 
 //delete
-exports.player_delete_destroy = async (req,res)=>{
+exports.player_delete_destroy = async (req, res) => {
   const player = await Player.findByIdAndDelete(req.params.playerId)
   res.redirect(`/leagues/${req.params.leagueId}/teams/${req.params.teamId}`)
 }
 
 //edit
-exports.player_show_edit = async (req,res)=>{
+exports.player_show_edit = async (req, res) => {
   const league = await League.findById(req.params.leagueId)
   const team = await Team.findById(req.params.teamId)
   const player = await Player.findById(req.params.playerId)
-  res.render('player/edit.ejs', {player,league, team})
+  res.render("player/edit.ejs", { player, league, team })
 }
-exports.player_update_put = async (req,res)=>{
+exports.player_update_put = async (req, res) => {
   const league = await League.findById(req.params.leagueId)
   const team = await Team.findById(req.params.teamId)
-  const player =await Player.findByIdAndUpdate(req.params.playerId, req.body)
-  res.redirect(`/leagues/${req.params.leagueId}/teams/${req.params.teamId}/players/${req.params.playerId}`)
+  const player = await Player.findByIdAndUpdate(req.params.playerId, req.body)
+  res.redirect(
+    `/leagues/${req.params.leagueId}/teams/${req.params.teamId}/players/${req.params.playerId}`
+  )
+}
+
+// trading player
+
+exports.trade_player_put = async (req, res) => {
+  console.log("hello")
+  const league = await League.findById(req.params.leagueId)
+
+  const currentTeam = await league.teams.find((team) => {
+    team._id
+  })
+
+  const newTeam = await league.teams.find((team) => {
+    team.req.body.trade_team
+  })
+
+  const player = await Player.findById(req.params.playerId)
+
+  newTeam.players.push(player._id)
+  await newTeam.save()
+
+  currentTeam.players.pull(player._id)
+  await currentTeam.save()
+
+  res.redirect(`/leagues/${req.params.leagueId}/teams/${req.params.teamId}`)
 }
